ec4d485d006cd083b84d7a14e89546e25a0e4e1b
Gafarias <gabriel_ff@outlook.com>
2022-11-24 15:28:18 -0300

    finished project

--- src/components/LocaleSwitcher.vue
+++ src/components/LocaleSwitcher.vue
@@ -1,7 +1,29 @@
 <template>
-    <button @click="$i18n.locale = locales[0]">Português</button>
-    <button @click="$i18n.locale = locales[1]">Inglês</button>
-    <button @click="$i18n.locale = locales[2]">Espanhol</button>
+    <nav class="mx-auto flex">
+        <button 
+          @click="$i18n.locale = locales[0]"
+          class="transition-all hover:brightness-90 mr-4 box-border w-32 h-12 bg-dark-500 flex items-center justify-center rounded-2xl text-white-900"
+        >
+            <img class="mr-1" src="@/assets/icons/brazil.svg" alt=""/>
+            Português
+        </button>
+
+        <button 
+          @click="$i18n.locale = locales[1]"
+          class="transition-all hover:brightness-90 mr-4 box-border w-32 h-12 bg-dark-500 flex items-center justify-center rounded-2xl text-white-900"
+        >
+            <img class="mr-1" src="@/assets/icons/english.svg" alt=""/>
+            Inglês
+        </button>
+
+        <button 
+          @click="$i18n.locale = locales[2]"
+          class="transition-all hover:brightness-90 mr-4 box-border w-32 h-12 bg-dark-500 flex items-center justify-center rounded-2xl text-white-900"
+        >
+            <img class="mr-1" src="@/assets/icons/spanish.svg" alt=""/>
+            Espanhol
+        </button>
+    </nav>
 </template>
 
 <script>
